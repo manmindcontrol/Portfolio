213ae690a7a17c869f2477d5dbfa2b15b6bc1bf5
Samuel Rychvalský <srychvalsky28@gmail.com>
2025-07-14 10:35:35 +0200

    samuel (master): background change

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Image from "next/image";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,10 +27,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`relative bg-cover bg-no-repeat bg-fixed bg-center overflow-x-hidden antialiased ${geistSans.variable} ${geistMono.variable}`}
+        className={`relative overflow-x-hidden antialiased ${geistSans.variable} ${geistMono.variable}`}
       >
+        {/* Background image */}
+        <div className="fixed inset-0 -z-10">
+          <Image
+            src="/abstract.png"
+            alt="background"
+            fill
+            sizes="100vw"
+            priority
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+
         <Navbar />
         {children}
+
         <footer className="py-8 text-center bg-black text-gray-400">
           <p>
             © {new Date().getFullYear()} Samuel Rychvalský. All rights reserved.
